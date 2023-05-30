766a7c8b9ac4d714308c8b6224b4887cfd726415
NikolaiInk <59829160+NikolaiInk@users.noreply.github.com>
2023-05-30 23:32:45 +0200

    Esben

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 
 use ml\laege\Controllers\VaccineController;
-use Gettext\{GettextTranslator, TranslatorFunctions};
+
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use ml\laege\fix\laegephplib\Feverspots;
-use fix\db\PDO; 
 use Carbon\Carbon;
 
 $dotenv = \Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
-$t = new GettextTranslator();
-$t->setLanguage('da_DK');
-$t->loadDomain('users', 'locale');
+
 
 Flight::route('/', function(){
-  $Vaccines = VaccineController::getAllVacc();
-  Flight::render('frontpage', array('body' => $Vaccines), 'body_content');
+  Flight::render('frontpage', array('body'), 'body_content');
   Flight::render('layout', array('title' => 'Home Page'));
  });
 
@@ -27,12 +22,5 @@ Flight::route('/', function(){
    Flight::render('login', array('body'), 'body_content');
    Flight::render('layout', array('title' => 'Login Page'));
 });
-
-// Flight::route('/api', function() {
-//     $url = 'http://localhost/MitLaegehus/api';
-//     $response = file_get_contents($url);
-//     Flight::json(json_decode($response));
-// });
-
 
 Flight::start();
